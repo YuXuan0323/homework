f402c9faa7c7377757acc6544ce92c21d6cdab2e
yushiuan <vivian32364@gmail.com>
2025-04-06 21:47:28 +0800

    Finished cover animation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
-
 $(document).ready(function(){
-    $('.accordion-title').click(function(){
-        $(".accordion-content").slideUp();
-        $('#accordion-item').slideToggle();
+    $('.accordion-item').hide();
+    $("input[id^='qa']").on('change', function (event) {
+        var target = $(event.target);
+        var accordion_item = target.next('label').next('.accordion-item');
+        accordion_item.slideToggle();
+
+        if (target.is(':checked')) {
+            target.next().addClass('qa-title');
+            // label.addClass("qa-title");
+        } else {
+            target.next().removeClass('qa-title');
+            // label.removeClass("qa-title");
+        }
     });
 });
